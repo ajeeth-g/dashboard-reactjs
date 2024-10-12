42f27a00113b175c6b76d1832e05e6ab281b9bcc
ajeeth-g <ajeetheswari14@gmail.com>
2024-10-12 18:29:13 +0530

    order section updated

--- src/components/orderContainer/OrderContainer.jsx
+++ src/components/orderContainer/OrderContainer.jsx
@@ -1,15 +1,15 @@
-import './orderContainer.css';
-import { orderData } from '../../util';
+import "./orderContainer.css";
+import { orderData } from "../../util";
 
 const OrderContainer = () => {
   return (
-    <div className='table-container'>
-      <div className='order-table'>
-        <div className='table-title'>
-          <p className='table-title-order-id'>
-            Order No - <span className='order-id'>#SPK-1023</span>
+    <div className="table-container">
+      <div className="order-table">
+        <div className="table-title">
+          <p className="table-title-order-id">
+            Order No - <span className="order-id">#SPK-1023</span>
           </p>
-          <p className='table-title-date'>Estimated Delivery: 14, Oct 2024</p>
+          <p className="table-title-date">Estimated Delivery: 14, Oct 2024</p>
         </div>
         <table>
           <thead>
@@ -24,28 +24,54 @@ const OrderContainer = () => {
           <tbody>
             {orderData.map((item) => (
               <tr key={item.id}>
-                <td className='item-column'>
+                <td className="item-column">
                   <img
                     src={item.image}
                     alt={item.name}
-                    className='item-image'
+                    className="item-image"
                   />
-                  <div className='item-details'>
-                    <p className='item-name'>{item.name}</p>
-                    <p className='item-detail'>{item.dialSize}</p>
-                    <div className='item-offer'>
-                    <p className='item-detail'>{item.color}</p> <span>hi</span>
+                  <div className="item-details">
+                    <p className="item-name">{item.name}</p>
+                    <p className="item-detail">{item.dialSize}</p>
+                    <div className="item-offer">
+                      <p className="item-detail">{item.color}</p>{" "}
+                      {item.offer && <p className="offer-tag">In Offer</p>}
                     </div>
                   </div>
                 </td>
-                <td className='trackingNo'>{item.trackingNo}</td>
-                <td className='price'>${item.price}</td>
-                <td className='quantity'>{item.quantity}</td>
-                <td className='totalPrice'>${item.totalPrice}</td>
+                <td className="trackingNo">
+                  <p>{item.trackingNo}</p>
+                </td>
+                <td className="price">
+                  <p>{item.price}</p>
+                </td>
+                <td className="quantity">
+                  <p>{item.quantity}</p>
+                </td>
+                <td className="totalPrice">
+                  <p>{item.totalPrice}</p>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
+        <div className="order-amount-details">
+          <p>
+            Total Items: <span>04</span>
+          </p>
+          <p>
+            Sub Total: <span>$3,100</span>
+          </p>
+          <p>
+            Applied Coupon: <span>SPK12</span>
+          </p>
+          <p>
+            Delivery Fees: <span>-$29</span>
+          </p>
+          <p>
+            Total Saved: <span>$3,799</span>
+          </p>
+        </div>
       </div>
     </div>
   );
